972370c944e80e530d6358a6e30a71e6ecad31e6
Thijs Lausegger <326550@student.mboutrecht.nl>
2019-11-26 10:18:35 +0100

    no message

---
+++ Assets/scripts/MoveForward.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //hierdoor kan je character lopen
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 15;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //dit zorgt ervoor dat de dieren random spawnen
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        // dit is gelinkt aan de start het zorgt ervoor dat de dieren zoiezo spawnen
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+            0, spawnPosZ);
+        int animalIndex = Random.Range(0, this.animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos,
+            animalPrefabs[animalIndex].transform.rotation);
+    } 
+}
+
